285362e52a8c7c68f94a0c346d0553ecb2cd70bf
kcsabin6 <65354977+kcsabin6@users.noreply.github.com>
2020-10-05 23:58:27 -0400

    eureka-server: microservices

---
+++ src/main/java/com/cubic/it/springeurekaserver/wer.java
@@ -0,0 +1,8 @@
+package com.cubic.it.springeurekaserver;
+
+public class wer {
+public static void main(String[] args) {
+    int pwowo=Runtime.getRuntime().availableProcessors();
+    System.out.println(pwowo);
+}
+}
